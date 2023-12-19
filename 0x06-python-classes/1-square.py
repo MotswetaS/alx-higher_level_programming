69652bcc08c1c82a6f72d14e42996f796bb9d642
rebonye <motsweta04@gmail.com>
2023-12-19 05:09:19 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a square module"""
+
+
+class Square:
+    """defines a square"""
+
+
+def __init__(self, size):
+    """Is a constructor.
+    Args:
+    size: side of the square length.
+    """
+    self.__size = size
