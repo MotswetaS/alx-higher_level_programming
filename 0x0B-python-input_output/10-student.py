dc129d4f808d71a0b3c81535a7f7f76d213c551c
rebonye <motsweta04@gmail.com>
2024-01-09 11:50:07 -0800

    fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,6 +14,6 @@ class Student:
         """will get a dictionary representation of the student"""
 
         if (type(attrs) == list and
-                all(type(ele)==str for ele in attrs)):
+                all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
